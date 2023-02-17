6f53e43b4d7d610cb7e98d8a4855b8ba410fe740
Here East <nastiapiven@gmail.com>
2023-02-17 18:27:42 +0100

    improve image load

--- js/loader.js
+++ js/loader.js
@@ -1,23 +1,31 @@
 import { createSpans, glitchLetters } from "./glitch.js";
+export { hideLoader, isLoaderHidden, loadElement, loaderText };
 
 const loader = document.querySelector(".load__overlay");
 const loadElement = document.querySelector(".loader");
 const loaderText = loadElement.textContent;
 
+let isLoaderHidden = false;
+
+//////////////
+
 createSpans(loadElement, loaderText);
 glitchLetters(loadElement, loaderText, 1.5);
 
+// let interval = setInterval(() => {
+//     if(isLoaderFinished()) {
+//         hideLoader();
+//         clearInterval(interval);
+//     }
+// }, 500)
 
-let interval = setInterval(() => {
+// Finished loader
+function isLoaderFinished() {
     const spans = Array.from(loadElement.querySelectorAll("span"));
     const spansText = spans.map(span => span.textContent).join("");
 
-    if(spansText === loaderText) {
-        hideLoader();
-        clearInterval(interval);
-    }
-}, 500)
-
+    return spansText === loaderText;
+}
 
 // Hide loader
 function hideLoader() {
@@ -27,11 +35,16 @@ function hideLoader() {
         delay: 0.5,
         y: "-100%",
         ease: Power4.easeOut,
-        duration: 2
-    })
-
-    .from(".image__section", {
-        y: window.innerHeight,
         duration: 2,
-    }, "-=2")
+    })
+    .from(
+        ".image__section",
+        {
+            y: window.innerHeight,
+            duration: 1.5,
+        },
+        "-=2"
+    );
+
+    gsap.delayedCall(2, () => isLoaderHidden = true);
 }
