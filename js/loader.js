b1318065472141f66374958f6bf2c6e5507fb6eb
Here East <nastiapiven@gmail.com>
2023-05-23 13:17:51 +0200

    refactor: refactor code

--- js/loader.js
+++ js/loader.js
@@ -1,5 +1,5 @@
-import {createSpans, glitchLetters} from "./glitch.js";
-export {hideLoader, isLoaderHidden, loadElement, loaderText};
+import { createSpans, glitchLetters } from "./glitch.js";
+export { hideLoader, isLoaderHidden, loadElement, loaderText };
 
 const loader = document.querySelector(".load__overlay");
 const loadElement = document.querySelector(".loader");
@@ -14,24 +14,23 @@ glitchLetters(loadElement, loaderText, 3);
 
 // HIDE LOADER
 function hideLoader() {
-    const tl = gsap.timeline();
+  const tl = gsap.timeline();
 
-    tl.to(loader, {
-        delay: 1,
-        y: "-100%",
-        ease: Power4.easeOut,
-        duration: 2,
+  tl.to(loader, {
+    delay: 1,
+    y: "-100%",
+    ease: Power4.easeOut,
+    duration: 2
+  }).from(
+    ".image__section",
+    {
+      y: window.innerHeight,
+      duration: 1.8
+    },
+    "-=2"
+  );
 
-    }).from(
-        ".image__section",
-        {
-            y: window.innerHeight,
-            duration: 1.8,
-        },
-        "-=2"
-    );
-
-    setTimeout(() => {
-        isLoaderHidden = true;
-    }, 3000);
+  setTimeout(() => {
+    isLoaderHidden = true;
+  }, 3000);
 }
